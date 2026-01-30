ebc26802eb30ddb4dbd086b95c89e4189ac7a56a
krish-maurya <mauryakrish734@gmail.com>
2026-01-30 14:49:19 +0530

    deploying

--- server/src/server.ts
+++ server/src/server.ts
@@ -35,7 +35,5 @@ app.use('/api/blog', blogRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
 
