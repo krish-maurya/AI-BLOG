3f0fe67c51c0cbb130b27c2acdd4e70c704efa08
krish-maurya <mauryakrish734@gmail.com>
2026-01-31 14:09:49 +0530

    login

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import auth from "../middleware/auth.js";
 import { userCreate, userLogin } from "../controllers/userController.js";
 
 const userRouter = express.Router();
--- server/src/server.ts
+++ server/src/server.ts
@@ -37,6 +37,6 @@ app.use('/api/user', userRouter);
 
 const PORT = process.env.PORT || 5000;
 
-
+// app.listen(PORT)
 export default app;
 
